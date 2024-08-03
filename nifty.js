ed57a6976e4daff8a6c904c5bc61812dd866fa20
Shashi <95562773+imshashi365@users.noreply.github.com>
2024-08-03 16:12:37 +0530

    Add files via upload

---
+++ nifty.js
@@ -0,0 +1,27 @@
+function openMenu() {
+    document.getElementById("myMenu").style.width = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("myMenu").style.width = "0";
+}
+
+
+function calculateLevels() {
+    var inputNumber = document.getElementById("numberInput").value;
+    var messageElement = document.getElementById("message");
+
+    if (inputNumber.length === 5) {
+        var number = parseFloat(inputNumber);
+        var supportLevel = Math.pow(Math.sqrt(number) - 0.25, 2);
+        var resistanceLevel = Math.pow(Math.sqrt(number) + 0.25, 2);
+
+        document.getElementById("support").textContent = supportLevel.toFixed(2);
+        document.getElementById("resistance").textContent = resistanceLevel.toFixed(2);
+        messageElement.textContent = '';
+    } else {
+        messageElement.textContent = 'Enter appropriate Levels';
+        document.getElementById("support").textContent = '';
+        document.getElementById("resistance").textContent = '';
+    }
+}
